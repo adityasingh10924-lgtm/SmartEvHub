acb74cb25fc7d33b2adac1a6293226165dfb25c7
adityasingh10924-lgtm <adityasingh10924@gmail.com>
2026-09-06 23:21:31 +0530

    add database and backend module

---
+++ backend/include/Database.h
@@ -0,0 +1,73 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "sqlite3.h"
+#include <string>
+
+class Database {
+private:
+    sqlite3* db;
+
+public:
+    Database();
+    ~Database();
+
+    bool connect(const std::string& databasePath);
+    void disconnect();
+
+    bool registerUser(
+        const std::string& name,
+        const std::string& email,
+        const std::string& password
+    );
+    
+    bool loginUser(
+       const std::string& email,
+       const std::string& password
+    );
+
+    int getLoggedInUserID(
+    const std::string& email,
+    const std::string& password
+     );
+
+    bool addVehicle(
+       int userID,
+       const std::string& vehicleNumber,
+       const std::string& vehicleType,
+       double batteryCapacity
+    );
+
+     void getVehicles(int userID);
+
+    bool addChargingStation(
+    const std::string& stationName,
+    const std::string& location,
+    int totalSlots,
+    int availableSlots,
+    const std::string& status
+    );
+
+    void getChargingStations();
+
+    bool createChargingRequest(
+    int userID,
+    int vehicleID,
+    int stationID
+    );
+     
+    void getChargingRequests();
+
+    bool updateRequestStatus(
+    int requestID,
+    const std::string& newStatus
+    );
+
+    bool completeChargingRequest(
+    int requestID,
+    double energyCharged
+);
+    void getTransactions(int userID);
+};
+
+#endif
